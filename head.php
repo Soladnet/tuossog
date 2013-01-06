8abc23672f66467255fed5239009aee3883da1af
Abbaty [Muhammad] Kori <muhammad.kori@gmail.com>
2013-01-06 06:30:41 -0800

    Experimental font

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" media="screen" href="css/style.css">
 <link rel="shortcut icon" href="favicon.ico">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" media="screen" rel="stylesheet" type="text/css">
 <title>Gossout - </title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />  
