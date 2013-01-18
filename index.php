cf41fd8728f52aa8587c49ae9ea079d450b9c6eb
Abbaty [Muhammad] Kori <muhammad.kori@gmail.com>
2013-01-18 12:13:53 +0100

    Index Page (no-icons yet)

--- index.php
+++ index.php
@@ -6,37 +6,112 @@
 	?>
 </head>
 <body>
-	<div class="page-wrapper">
-		<?php
-		include ("nav.php");
-		?>
-		<div class="content">
-			<script type="text/javascript">
-			function showHide(shID) {
-				if (document.getElementById(shID)) {
-					if (document.getElementById(shID+'-show').style.display != 'none') {
-						document.getElementById(shID+'-show').style.display = 'none';
-						document.getElementById(shID).style.display = 'block';
-					}
-					else {
-						document.getElementById(shID+'-show').style.display = 'inline';
-						document.getElementById(shID).style.display = 'none';
-					}
-				}
-			}
-			</script>
-			<?php 
-			include("new-post.php");
-			include("new-post.php");
-			include("new-post.php");
-			?>	
-			<div class="clear"></div>		
+	<div class="index-page-wrapper">	
+		<div class="index-intro">
+			<div class="index-nav">
+				<img src="images/gossout-logo-text-and-image-svg.svg" height="100px">
+				<h1>Gossout is a Community Based blah blah blah.....</h1>
+			</div>
+			<p>
+				<hr>
+				<span>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. 
+				</span>
+				<h3>Learn more...</h3>
+				<div class="clear"></div>
+			</p>
 		</div>
-		
-		<?php
-		include("footer.php");
-		?>
-	</div>
+		<div class="index-content">
+			<div class="index-content-divs">
+				<p>
+					<h1 class="first">Share how you want, when you want</h1>
+					<hr>
+					There are lots of way to share with your communities. 
+					Get home welding tips from your DIY pals in a hangout, 
+					share restaurant reviews with fellow foodies, or use Google+ 
+					Events to plan your next block party. You can share with communities 
+					from anywhere you find Google+: on YouTube, Google Maps, or through the +1 button on sites across the web.
 
-</body>
-</html>
+					<h1>Gather together</h1>
+					<hr>
+					Whether it's for your family, your book club or people around the world who 
+					share your interests, communities give each of your different 
+					groups a home base.
+
+					<h1>Discover new things</h1>
+					<hr>
+					One idea can spawn many more, so you'll always find a 
+					fascinating community to join.
+
+					<h1>Join public communities</h1>
+					<hr>
+					No matter what you're into, there are people on Gossout 
+					you can talk with about it. Search for public communities 
+					around the topics you're passionate about and join the conversation. 
+					You'll be learning new things in no time.
+
+					<h1>Connect with others</h1>
+					<hr>
+					Invite your friends and followers, and meet others who share your interests.
+				</p>
+			</div>
+			<div class="index-content-divs">
+
+			</div>
+
+		</div>
+		<div class="index-aside">
+			<div class="index-aside-divs">
+				<p>
+					<h3>Explore</h3>
+					<h4>Communities on Gossout</h4>
+					<hr>
+					[copied from Google+] From foodies, to volunteer groups, to football fanatics,
+					communities help you find people who are into the same things you are. 
+					Share news, ideas and passions with just the right people, or meet some
+					new ones by joining communities.</p>
+					<h5>Learn more...</h5>
+					<div class="clear"></div>
+				</div>
+				<div class="index-aside-divs">
+					<p>
+						<h3>Search</h3>
+						<h4>Communities, Tags, People</h4>
+						<hr>
+						[copied from S101] Connect with others, Invite your friends, 
+						and meet others who share your interests </p>
+						<input type="" class="index-search" placeholder="Search for Communities, Tags, People">
+					</div>
+					<div class="index-aside-divs">
+						<p>
+							<h3>Trending Tags</h3>
+							<hr>
+							<span class="tag"><a href="">#Lorem</a></span>
+							<span class="tag"><a href="">#ipsum</a></span>
+							<span class="tag"><a href="">#consectetur</a></span>
+							<span class="tag"><a href="">#adipisicing</a></span>
+							<span class="tag"><a href="">#eiusmod</a></span>
+							<span class="tag"><a href="">#tempor</a></span>
+							<span class="tag"><a href="">#incididunt</a></span>
+							<span class="tag"><a href="">#labore</a></span>
+							<span class="tag"><a href="">#dolore</a></span>
+							<span class="tag"><a href="">#magna</a></span>
+							<span class="tag"><a href="">#aliqua</a></span>
+							<span class="tag"><a href="">#veniam</a></span>
+							<span class="tag"><a href="">#nostrud</a></span>
+							<span class="tag"><a href="">#exercitation</a></span>
+							<span class="tag"><a href="">#ullamco</a></span>
+							<span class="tag"><a href="">#laboris</a></span>
+							<span class="tag"><a href="">#reprehenderit</a></span>
+						</p>
+						<h5>See more...</h5>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<?php
+				include("footer.php");
+				?>
+			</div>
+		</body>
+		</html>
