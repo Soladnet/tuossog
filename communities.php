aabc8b14b296fd88114a37dfc1e491854bed98d2
Alkali Ibrahim <alkalibrahim@gmail.com>
2013-01-19 04:27:07 +0100

    Community pages

--- communities.php
+++ communities.php
@@ -69,25 +69,66 @@
 
 				<div class="community-box">
 					<div class="community-box">
-						<div><a href="sample-community.php">Eko Radio Party</a> </div>
-						<div class="details">ERP, a radio programme like no other... 
-							  with sole objective of making serious issues light. We educate and entertain
+						<div class="community-box-wrapper">
+						<div class="community-image">
+							<img src="images/1.jpg">
 						</div>
-						<div class="members">200 Members</div>
-						<div class="members">200 Posts</div>
-						<div class="clear"></div>
+						<div class="community-text">
+							<div class="community-name"><a href="sample-community.php">Eko Radio Party</a> </div>
+							<div class="details">ERP, a radio programme like no other... 
+								  with sole objective of making serious issues light. We educate and entertain.
+
+							</div>
+							<div class="members">200 Members</div>
+							<div class="members">200 Posts</div>
+							
+						</div>
+						
 						<!-- <span class="button communities-button" onclick="openChat();"><span class="icon-16-chat"></span> Chat</span>
 						<span class="button communities-button"><span class="icon-16-checkmark"></span> Join </span> -->
+						</div>
 					</div>
 					<div class="community-box">
-						<div><a href="sample-community.php">Kiev National University of Construction And Architecture</a> </div>
+						<div class="community-box-wrapper">
+						<div class="community-image">
+							<img src="images/1.jpg">
+						</div>
+						<div class="community-text">
+							<div class="community-name"><a href="sample-community.php">Eko Radio Party</a> </div>
+							<div class="details">ERP, a radio programme like no other... 
+								  with sole objective of making serious issues light. We educate and entertain.
+
+							</div>
+							<div class="members">200 Members</div>
+							<div class="members">200 Posts</div>
+							
+						</div>
 						
-						<div class="members">200 Members</div>
-						<div class="members">200 Posts</div>
+						<!-- <span class="button communities-button" onclick="openChat();"><span class="icon-16-chat"></span> Chat</span>
+						<span class="button communities-button"><span class="icon-16-checkmark"></span> Join </span> -->
+						</div>
+					</div>
+					<div class="community-box">
+						<div class="community-box-wrapper">
+						<div class="community-image">
+							<img src="images/eset.jpg">
+						</div>
+						<div class="community-text">
+							<div class="community-name"><a href="sample-community.php">The Pennsylvania State University New Kensington Campus of the Commonwealth College</a> </div>
+							<div class="details">
+
+							</div>
+							<div class="members">200 Members</div>
+							<div class="members">200 Posts</div>
+							
+						</div>
 						<div class="clear"></div>
 						<!-- <span class="button communities-button" onclick="openChat();"><span class="icon-16-chat"></span> Chat</span>
 						<span class="button communities-button"><span class="icon-16-checkmark"></span> Join </span> -->
+						</div>
 					</div>
+
+					
 					
 
 				</div>
