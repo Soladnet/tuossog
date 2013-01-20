5e4679fa9b5538bd80a86de90e14ea43daf6e5d5
Abbaty [Muhammad] Kori <muhammad.kori@gmail.com>
2013-01-20 16:52:25 +0100

    Index Page
    Also, reversal of fonts to our default fonts used in Gossout v1.0

--- index.php
+++ index.php
@@ -7,111 +7,134 @@
 </head>
 <body>
 	<div class="index-page-wrapper">	
+		<div class="index-nav">
+			<span class="index-login">Already have an account? <a href="home.php">Login Here!</a></span>
+			<div class="clear"></div>
+		</div>
+		<div class="index-banner">
+			<div class="index-logo">
+				<img src="images/gossout-logo-text-and-image-svg.svg" alt="logo" height="200px">
+			</div>
+		</div>
 		<div class="index-intro">
-			<div class="index-nav">
-				<img src="images/gossout-logo-text-and-image-svg.svg" height="100px">
-				<h1>Gossout is a Community Based blah blah blah.....</h1>
-			</div>
-			<p>
+			<div class="index-intro-1">
+				<h2>
+					What if we had a headline so punchy that 
+					no one could refuse to click our button? 
+				</h2>
 				<hr>
-				<span>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. 
-				</span>
-				<h3>Learn more...</h3>
-				<div class="clear"></div>
-			</p>
+			</div>
 		</div>
-		<div class="index-content">
-			<div class="index-content-divs">
-				<p>
-					<h1 class="first">Share how you want, when you want</h1>
-					<hr>
-					There are lots of way to share with your communities. 
-					Get home welding tips from your DIY pals in a hangout, 
-					share restaurant reviews with fellow foodies, or use Google+ 
-					Events to plan your next block party. You can share with communities 
-					from anywhere you find Google+: on YouTube, Google Maps, or through the +1 button on sites across the web.
-
-					<h1>Gather together</h1>
-					<hr>
-					Whether it's for your family, your book club or people around the world who 
-					share your interests, communities give each of your different 
-					groups a home base.
-
-					<h1>Discover new things</h1>
-					<hr>
-					One idea can spawn many more, so you'll always find a 
-					fascinating community to join.
-
-					<h1>Join public communities</h1>
-					<hr>
-					No matter what you're into, there are people on Gossout 
-					you can talk with about it. Search for public communities 
-					around the topics you're passionate about and join the conversation. 
-					You'll be learning new things in no time.
-
-					<h1>Connect with others</h1>
-					<hr>
-					Invite your friends and followers, and meet others who share your interests.
+			<div class="index-intro-2">
+				<p>And if our headline wasn't good
+					enough, what if our secondary messaging 
+					was here to further push our value proposition?
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+					Sapiente similique magnam error repudiandae in tempore iusto libero porro 
+					fugit nemo. Minima quia odio saepe ea blanditiis labore molestias dolores impedit.
 				</p>
+				<a href="" class="button-big">Sign up now!</a>
+				<div class="clear"></div>
 			</div>
-			<div class="index-content-divs">
+		<div class="index-shadow-bottom"></div>
+		<div class="index-content-wrapper">
+			<div class="index-content">
+				<div class="index-content-divs">
+					<p>
+						<h2 class="first">Share how you want, when you want</h2>
+						<hr>
+						There are lots of way to share with your communities. 
+						Get home welding tips from your DIY pals in a hangout, 
+						share restaurant reviews with fellow foodies, or use Google+ 
+						Events to plan your next block party. You can share with communities 
+						from anywhere you find Google+: on YouTube, Google Maps, or through the +1 button on sites across the web.
 
-			</div>
+						<h2>Gather together</h2>
+						<hr>
+						Whether it's for your family, your book club or people around the world who 
+						share your interests, communities give each of your different 
+						groups a home base.
 
-		</div>
-		<div class="index-aside">
-			<div class="index-aside-divs">
-				<p>
-					<h3>Explore</h3>
-					<h4>Communities on Gossout</h4>
-					<hr>
-					[copied from Google+] From foodies, to volunteer groups, to football fanatics,
-					communities help you find people who are into the same things you are. 
-					Share news, ideas and passions with just the right people, or meet some
-					new ones by joining communities.</p>
-					<h5>Learn more...</h5>
-					<div class="clear"></div>
+						<h2>Discover new things</h2>
+						<hr>
+						One idea can spawn many more, so you'll always find a 
+						fascinating community to join.
+
+						<h2>Join public communities</h2>
+						<hr>
+						No matter what you're into, there are people on Gossout 
+						you can talk with about it. Search for public communities 
+						around the topics you're passionate about and join the conversation. 
+						You'll be learning new things in no time.
+
+						<h2>Connect with others</h2>
+						<hr>
+						Invite your friends and followers, and meet others who share your interests.
+					</p>
 				</div>
+				<div class="index-content-divs">
+
+				</div>
+
+			</div>
+			<div class="index-aside">
 				<div class="index-aside-divs">
 					<p>
-						<h3>Search</h3>
-						<h4>Communities, Tags, People</h4>
+						<h3>Explore</h3>
+						<h4>Communities on Gossout</h4>
 						<hr>
-						[copied from S101] Connect with others, Invite your friends, 
-						and meet others who share your interests </p>
-						<input type="" class="index-search" placeholder="Search for Communities, Tags, People">
+						[copied from Google+] From foodies, to volunteer groups, to football fanatics,
+						communities help you find people who are into the same things you are.</p>
+						<h5>Learn more...</h5>
+						<div class="clear"></div>
 					</div>
 					<div class="index-aside-divs">
 						<p>
-							<h3>Trending Tags</h3>
+							<h3>Search</h3>
+							<h4>Communities, Tags, People</h4>
 							<hr>
-							<span class="tag"><a href="">#Lorem</a></span>
-							<span class="tag"><a href="">#ipsum</a></span>
-							<span class="tag"><a href="">#consectetur</a></span>
-							<span class="tag"><a href="">#adipisicing</a></span>
-							<span class="tag"><a href="">#eiusmod</a></span>
-							<span class="tag"><a href="">#tempor</a></span>
-							<span class="tag"><a href="">#incididunt</a></span>
-							<span class="tag"><a href="">#labore</a></span>
-							<span class="tag"><a href="">#dolore</a></span>
-							<span class="tag"><a href="">#magna</a></span>
-							<span class="tag"><a href="">#aliqua</a></span>
-							<span class="tag"><a href="">#veniam</a></span>
-							<span class="tag"><a href="">#nostrud</a></span>
-							<span class="tag"><a href="">#exercitation</a></span>
-							<span class="tag"><a href="">#ullamco</a></span>
-							<span class="tag"><a href="">#laboris</a></span>
-							<span class="tag"><a href="">#reprehenderit</a></span>
-						</p>
-						<h5>See more...</h5>
-						<div class="clear"></div>
+							[copied from S101] Connect with others, Invite your friends, 
+							and meet others who share your interests </p>
+							<input type="" class="index-search" placeholder="Search for Communities, Tags, People">
+						</div>
+						<div class="index-aside-divs">
+							<p>
+								<h3>Trending Tags</h3>
+								<h4></h4>
+								<hr>
+								<p>
+									The # symbol, called a hashtag, is used to mark keywords or topics.
+									It was created organically by Twitter users as a way to categorize messages.
+									Clicking on a hashtagged word in any message shows you 
+									all other Posts marked with that keyword.
+								</p>
+								<span class="tag"><a href="">#Lorem</a></span>
+								<span class="tag"><a href="">#ipsum</a></span>
+								<span class="tag"><a href="">#consectetur</a></span>
+								<span class="tag"><a href="">#adipisicing</a></span>
+								<span class="tag"><a href="">#eiusmod</a></span>
+								<span class="tag"><a href="">#tempor</a></span>
+								<span class="tag"><a href="">#incididunt</a></span>
+								<span class="tag"><a href="">#labore</a></span>
+								<span class="tag"><a href="">#dolore</a></span>
+								<span class="tag"><a href="">#magna</a></span>
+								<span class="tag"><a href="">#aliqua</a></span>
+								<span class="tag"><a href="">#veniam</a></span>
+								<span class="tag"><a href="">#nostrud</a></span>
+								<span class="tag"><a href="">#exercitation</a></span>
+								<span class="tag"><a href="">#ullamco</a></span>
+								<span class="tag"><a href="">#laboris</a></span>
+								<span class="tag"><a href="">#reprehenderit</a></span>
+							</p>
+							<h5>See more...</h5>
+							<div class="clear"></div>
+						</div>
 					</div>
+					<?php
+					include("footer.php");
+					?>
 				</div>
-				<?php
-				include("footer.php");
-				?>
+
 			</div>
 		</body>
 		</html>
