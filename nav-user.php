cf2fd599b17181840c7c53c8fbbd3ec5fe8eda84
Alkali Ibrahim <alkalibrahim@gmail.com>
2013-01-21 16:02:24 +0100

    Responsive nav

--- nav-user.php
+++ nav-user.php
@@ -1,8 +1,11 @@
 <div class="nav-user">
 	<ul>
-		<li><a href=""><span class="icon-16-list"></span><span>Gossbag</span></a></li>
-		<li><a href=""><span class="icon-16-mail"></span><span>Messages</span></a></li>
-		<li  class="last"><a href=""><span class="icon-16-user-add"></span><span>Friend Requests</span></a></li>
+		<li><a href=""><span class="icon-16-list"></span><span>Gossbag</span></a>
+			<span class="notifier">19</span>
+		</li>
+
+		<!-- <li><a href=""><span class="icon-16-mail"></span><span>Messages</span></a></li>
+		<li  class="last"><a href=""><span class="icon-16-user-add"></span><span>Friend Requests</span></a></li> -->
 		<li class="nav-user-profile last"><a href=""><span>Ciroma Chukwuma Adekunle</span><span class="icon-16-vcard"></span></a></li>
 	</ul>
 	<div class="clear"></div>
--- nav.php
+++ nav.php
@@ -4,6 +4,7 @@
 				<li><a href="home.php"><span class="icon-house"></span><span>Home</span></a></li>
 				<li class="last"><a href="communities.php"><span class="icon-contract"></span><span>Communities</span></a></li>		
 				<li class="nav-login last"><a href=""><span>Login</span><span class="icon-login"></span></a></li>
+				<!-- <li><a href=""><span class="icon-search"></span></a></li> -->
 				<li class="nav-search-container float-right">
 					<label for="nav-search"></label>
 					<input name="nav-search " placeholder="" type="text" value=""  required/>
