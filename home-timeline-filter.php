8f29bac4d92b76c1c9d1f67be38622b13924853f
Alkali Ibrahim <alkalibrahim@gmail.com>
2013-01-29 23:21:50 +0100

    Timeline filter
    Better idea

--- home-timeline-filter.php
+++ home-timeline-filter.php
@@ -1,10 +1,10 @@
 <div class="timeline-filter">
 	<ul>
-		
+		<li><span class="icon-16-chat"></span></li>
 		<li><a href="">All</a></li>
-		<li><a href=""><p>Friends</p> <p> Activities</p></a></li>
-		<li><a href=""><p>Community</p><p>Feeds</p></a></li>
-		<li class="active"><a href=""><p>Gossout</p><p> Broadcast</p></a></li>
+		<li><a href=""><p>Friends</p> </a></li>
+		<li><a href=""><p>Community</p></a></li>
+		<li class="active"><a href=""><p>Gossout</p></a></li>
 	</ul>
 </div>
 <div class="clear"></div>
