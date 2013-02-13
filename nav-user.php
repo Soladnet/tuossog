5c16af6013d01d6361f9780f826899522e3aa140
Abdulrasheed <soladnet@hotmail.com>
2013-02-13 17:16:46 +0100

    formating nav-user and nav
    I just formated these files ....no big deal

--- nav-user.php
+++ nav-user.php
@@ -1,121 +1,121 @@
 <div class="nav-user">
-	<ul>
-		<li>
-			<a href="javascript:toggle('pop-up-gossbag','gossbag');">
-				<span class="icon-16-list"></span>
-				<span id="gossbag">Gossbag</span>
-			</a>
-			<span class="notification-number">19</span>
-			<div class="notifications-container" id="pop-up-gossbag">
-				<div class="notifications">
-					<a href="javascript:toggle('pop-up-gossbag','gossbag');" class="float-right">
-						<span class="icon-16-cross"></span>Close 
-					</a>
-					<div class="clear"></div>
-					<div class="individual-notification">
-						<p> 
-							<span class="icon-16-user-add"></span> 
-							<span class="float-right"> 17 hrs </span>
-						</p>
-						<img class= "notification-icon" src="images/1.jpg">
-						<div class="notification-text"> 
-							<p class="name">Chiroma Chukwuma Adekunle </p>
-							<p>wants to add you</p>
-						</div>
-						<div class="clear"></div>
-						<hr>
-						 <a class="notification-actions">View</a>
-						 <a class="notification-actions">Accept</a>
-						 <div class="clear"></div>
-					</div>
+    <ul>
+        <li>
+            <a href="javascript:toggle('pop-up-gossbag','gossbag');">
+                <span class="icon-16-list"></span>
+                <span id="gossbag">Gossbag</span>
+            </a>
+            <span class="notification-number">19</span>
+            <div class="notifications-container" id="pop-up-gossbag">
+                <div class="notifications">
+                    <a href="javascript:toggle('pop-up-gossbag','gossbag');" class="float-right">
+                        <span class="icon-16-cross"></span>Close 
+                    </a>
+                    <div class="clear"></div>
+                    <div class="individual-notification">
+                        <p> 
+                            <span class="icon-16-user-add"></span> 
+                            <span class="float-right"> 17 hrs </span>
+                        </p>
+                        <img class= "notification-icon" src="images/1.jpg">
+                        <div class="notification-text"> 
+                            <p class="name">Chiroma Chukwuma Adekunle </p>
+                            <p>wants to add you</p>
+                        </div>
+                        <div class="clear"></div>
+                        <hr>
+                        <a class="notification-actions">View</a>
+                        <a class="notification-actions">Accept</a>
+                        <div class="clear"></div>
+                    </div>
 
-					<div class="individual-notification">
-						<p> <span class="icon-16-comment"></span> <span class="float-right"> 17 hrs </span></p>
-						
-						<img class= "notification-icon" src="images/1.jpg">
-						<div class="notification-text"> 
-							<p class="name">Chiroma Chukwuma Adekunle </p>
-							<p>commented on your post</p>
-							<p>"Ahmadu Bello University is..."</p>
-						</div>
-						<div class="clear"></div>
-						<hr>
-						 <a class="notification-actions">View</a>
-						 <div class="clear"></div>
-					</div>
+                    <div class="individual-notification">
+                        <p> <span class="icon-16-comment"></span> <span class="float-right"> 17 hrs </span></p>
 
-					<div class="individual-notification viewed-notification">
-						<p> <span class="icon-16-comment"></span> 
-							<span class="float-right"> 17 hrs </span>
-						</p>
-						<img class= "notification-icon" src="images/1.jpg">
-						<div class="notification-text"> 
-							<p class="name">Chiroma Chukwuma Adekunle </p>
-							<p>invites you to join community</p>
-							<p>"Ahmadu Bello University Zaria..."</p>
-						</div>
-						<div class="clear"></div>
-						<hr>
-						 <a class="notification-actions">View</a>
-						 <a class="notification-actions">Accept</a>
-						 <div class="clear"></div>
-					</div>
+                        <img class= "notification-icon" src="images/1.jpg">
+                        <div class="notification-text"> 
+                            <p class="name">Chiroma Chukwuma Adekunle </p>
+                            <p>commented on your post</p>
+                            <p>"Ahmadu Bello University is..."</p>
+                        </div>
+                        <div class="clear"></div>
+                        <hr>
+                        <a class="notification-actions">View</a>
+                        <div class="clear"></div>
+                    </div>
 
-					<a href=""> See all</a>
-				</div>
-			</div>
-			<div class="notifications-container" id="pop-up-message">
-				<div class="notifications">
-					<a href="javascript:toggle('pop-up-message','messages');" class="float-right">
-						<span class="icon-16-cross"></span>Close 
-					</a>
-						<div class="clear"></div>
-						<div class="individual-notification">
-						<p> 
-							<span class="float-right"> 17 hrs </span>
-							<div class="clear"></div>
-						</p>
-						<img class= "notification-icon" src="images/1.jpg">
-						<div class="notification-text"> 
-							<p class="name">Chiroma Chukwuma Adekunle </p>
-							<p><span class="icon-16-reply"></span>Message body...</p>
-						</div>
-						<div class="clear"></div>
-						<hr>
-						 <a class="notification-actions">View</a>
-						 <div class="clear"></div>
-					</div>
-					<div class="individual-notification viewed-notification">
-						<p>
-							<span class="float-right"> 17 hrs </span>
-							<div class="clear"></div>
-						</p>
-						<img class= "notification-icon" src="images/1.jpg">
-						<div class="notification-text"> 
-							<p class="name">Chiroma Chukwuma Adekunle </p>
-							<p>Message body</p>
-						</div>
-						<div class="clear"></div>
-						<hr>
-						 <a class="notification-actions">View</a>
-						 <div class="clear"></div>
-					</div>
+                    <div class="individual-notification viewed-notification">
+                        <p> <span class="icon-16-comment"></span> 
+                            <span class="float-right"> 17 hrs </span>
+                        </p>
+                        <img class= "notification-icon" src="images/1.jpg">
+                        <div class="notification-text"> 
+                            <p class="name">Chiroma Chukwuma Adekunle </p>
+                            <p>invites you to join community</p>
+                            <p>"Ahmadu Bello University Zaria..."</p>
+                        </div>
+                        <div class="clear"></div>
+                        <hr>
+                        <a class="notification-actions">View</a>
+                        <a class="notification-actions">Accept</a>
+                        <div class="clear"></div>
+                    </div>
 
-					<a href=""> See all</a>
+                    <a href=""> See all</a>
+                </div>
+            </div>
+            <div class="notifications-container" id="pop-up-message">
+                <div class="notifications">
+                    <a href="javascript:toggle('pop-up-message','messages');" class="float-right">
+                        <span class="icon-16-cross"></span>Close 
+                    </a>
+                    <div class="clear"></div>
+                    <div class="individual-notification">
+                        <p> 
+                            <span class="float-right"> 17 hrs </span>
+                        <div class="clear"></div>
+                        </p>
+                        <img class= "notification-icon" src="images/1.jpg">
+                        <div class="notification-text"> 
+                            <p class="name">Chiroma Chukwuma Adekunle </p>
+                            <p><span class="icon-16-reply"></span>Message body...</p>
+                        </div>
+                        <div class="clear"></div>
+                        <hr>
+                        <a class="notification-actions">View</a>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="individual-notification viewed-notification">
+                        <p>
+                            <span class="float-right"> 17 hrs </span>
+                        <div class="clear"></div>
+                        </p>
+                        <img class= "notification-icon" src="images/1.jpg">
+                        <div class="notification-text"> 
+                            <p class="name">Chiroma Chukwuma Adekunle </p>
+                            <p>Message body</p>
+                        </div>
+                        <div class="clear"></div>
+                        <hr>
+                        <a class="notification-actions">View</a>
+                        <div class="clear"></div>
+                    </div>
 
-				</div>
-			</div>
-		</li>
+                    <a href=""> See all</a>
 
-		<li class="last">
-		<a id="messages" href="javascript:toggle('pop-up-message','messages');" >
-				<span class="icon-16-mail"></span>
-				<span>Messages</span>
-			</a>
-			<span class="notification-number">15</span>
-		</li>
-		
-		<li class="nav-user-profile last"><a href=""><span>Ciroma Chukwuma Adekunle</span><span class="icon-16-vcard"></span></a></li>
-	</ul>
-	<div class="clear"></div>
+                </div>
+            </div>
+        </li>
+
+        <li class="last">
+            <a id="messages" href="javascript:toggle('pop-up-message','messages');" >
+                <span class="icon-16-mail"></span>
+                <span>Messages</span>
+            </a>
+            <span class="notification-number">15</span>
+        </li>
+
+        <li class="nav-user-profile last"><a href=""><span>Ciroma Chukwuma Adekunle</span><span class="icon-16-vcard"></span></a></li>
+    </ul>
+    <div class="clear"></div>
 </div>
--- nav.php
+++ nav.php
@@ -1,15 +1,15 @@
 <div class="nav">
-			<ul>
-				<!-- <img src="images/gossout-logo-image-svg.svg" alt="gossout-logo"> -->
-				<li><a href="home.php"><span class="icon-house"></span><span>Home</span></a></li>
-				<li class="last"><a href="communities.php"><span class="icon-contract"></span><span>Communities</span></a></li>		
-				<li class="nav-login last"><a href=""><span>Login</span><span class="icon-login"></span></a></li>
-				<!-- <li><a href=""><span class="icon-search"></span></a></li> -->
-				<li class="nav-search-container float-right">
-					<label for="nav-search"></label>
-					<input name="nav-search " placeholder="" type="text" value=""  required/>
-				</li>
-				<div class="clear"></div>
-			</ul>
-			<div class="clear"></div>
-		</div>				
+    <ul>
+            <!-- <img src="images/gossout-logo-image-svg.svg" alt="gossout-logo"> -->
+        <li><a href="home.php"><span class="icon-house"></span><span>Home</span></a></li>
+        <li class="last"><a href="communities.php"><span class="icon-contract"></span><span>Communities</span></a></li>		
+        <li class="nav-login last"><a href=""><span>Login</span><span class="icon-login"></span></a></li>
+        <!-- <li><a href=""><span class="icon-search"></span></a></li> -->
+        <li class="nav-search-container float-right">
+            <label for="nav-search"></label>
+            <input name="nav-search " placeholder="" type="text" value=""  required/>
+        </li>
+        <div class="clear"></div>
+    </ul>
+    <div class="clear"></div>
+</div>				
