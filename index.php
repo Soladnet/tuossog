e6d2d627104b9b9ddcf659833352877422cd1c79
Abbaty [Muhammad] Kori <muhammad.kori@gmail.com>
2013-04-13 14:01:29 +0100

    NEW Index Page

--- index.php
+++ index.php
@@ -18,45 +18,49 @@
 		</div>
 		<div class="index-intro">
 			<div class="index-intro-1">
-				<h1>
-					Join Communities and share stories, images, opinions, with people who share the same interests, locality, or school with you
-  <!-- <a href="suite101.com">www.suite101.com</a> -->
+				<h1>Join Communities and Share Stories, 
+					Photos, Opinions with People who Share the same 
+					Interests, Locality with you.
 				</h1>
+
+
+				<input class="main-search" type="text" placeholder="...">
+
+				<button class="button-big"><a href="">Search</a></button>
+				<div class="clear"></div>
+
 				<hr>
 			</div>
-		<div class="index-intro-2-container">
-			<div class="index-intro-2">	
-				<p>Gossout introduces a fun way of letting you know about things relevant to you. Go ahead and join Communities. You can create your own Community, invite friends and start sharing with them now.
-				</p>
-				<button class="button-big"><a href="signup-personal.php">Sign up now</a></button>
-				<div class="clear"></div>
-				<div class="index-functions">
-					<div style="margin: 0 auto; width: 24px;">
-						<span class="icon-globe"></span>
-					</div>
+			<div class="index-intro-2-container">
+				<div class="index-intro-2">	
+
+					<div class="index-functions">
+						<div style="margin: 0 auto; width: 24px;">
+							<span class="icon-globe"></span>
+						</div>
 						<h3>Discover</h3>
 						<p>Communities &amp; Friends</p>
-				</div>
-				
-				<div class="index-functions">
-					<div style="margin: 0 auto; width: 24px;">
-						<span class="icon-share"></span>
 					</div>
+
+					<div class="index-functions">
+						<div style="margin: 0 auto; width: 24px;">
+							<span class="icon-share"></span>
+						</div>
 						<h3>Connect</h3>
 						<p>Meet People, Share Interests</p>
-				</div>
+					</div>
 
-				<div class="index-functions">
+				<!-- <div class="index-functions">
 					<div style="margin: 0 auto; width: 24px;">
 						<span class="icon-search"></span>
 					</div>
 						<h3>Search</h3>
 						<p>Communities and Friends</p>
+					</div> -->
+
+					<div class="clear"></div>
 				</div>
-				
-				<div class="clear"></div>
-			</div>
-		</div>		
+			</div>		
 		</div>
 		<div class="index-shadow-bottom"></div>
 		<div class="index-content-wrapper">
