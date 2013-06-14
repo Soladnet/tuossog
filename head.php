fbfaea9288ba4243d448bca3a6d3db57502ce492
Alkali Ibrahim <alkalibrahim@gmail.com>
2013-06-14 13:18:06 +0100

    Speed Optimization : CSS references first before Javascript

--- head.php
+++ head.php
@@ -1,13 +1,12 @@
 <link rel="shortcut icon" href="favicon.ico">
-
+<link rel="stylesheet" media="screen" href="css/style.css">
+<link rel="stylesheet" href="css/hint.min.css">
+<link rel="stylesheet" href="css/libnotify.css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" src="scripts/script.js"></script>
 <script type="text/javascript" src="scripts/jquery.fancybox.pack.js?v=2.1.4"></script>
 <script type="text/javascript" src="scripts/humane.min.js"></script>
 <script src="scripts/jquery.sticky.js"></script>
-<link rel="stylesheet" media="screen" href="css/style.css">
-<link rel="stylesheet" href="css/hint.min.css">
-<link rel="stylesheet" href="css/libnotify.css">
 <script type="text/javascript" src="scripts/modernizr.custom.77319.js"></script>
 <title>Gossout - </title>
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" > 
