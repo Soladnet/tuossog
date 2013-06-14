9e797f75ca12edf514f15ca8981d472dd9aac004
Alkali Ibrahim <alkalibrahim@gmail.com>
2013-06-14 15:55:33 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+	<?php
+	include ("head.php");
+	?>
+</head>
+<body>
+	<div class="index-page-wrapper">	
+		<div class="index-nav">
+			<span class="index-login">Already have an account? <a href="home.php">Login Here</a></span>
+			<div class="clear"></div>
+		</div>
+		<div class="index-banner">
+			<div class="index-logo">
+				<img src="images/gossout-logo-text-and-image-svg.svg" alt="logo" >
+			</div>
+		</div>
+		<div class="index-intro">
+			<div class="index-intro-1">
+				<h1>Oops, we couldn't find the page you were looking for. Go to our <a href="home.php">homepage</a> or search for it below</h1>
+
+
+				<input class="main-search" type="text" placeholder="...">
+
+                                <span><center><button class="button-big"><a href="">Search</a></button> <!--<button class="button-big"><a href="">Sign up</a></button>--></center></span>
+				<div class="clear"></div>
+
+			</div>
+		</br>
+		</br>
+		</br>
+		
+		<div class="index-shadow-bottom"></div>
+		<div class="index-content-wrapper">
+			
+			<?php
+			include("footer.php");
+			?>
+		</div>
+
+	</div>
+</body>
+</html>
